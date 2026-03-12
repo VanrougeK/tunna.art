7e402ad2beb44d15e314569a7883ee35d8d9e8a3
VanrougeK <ochoavania795@gmail.com>
2026-03-12 13:43:00 -0600

    Desde cero
    Problemas con el repositorio pasado por problemas al deployar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-6xl text-pink-500 font-bold">
+        tunna.art
+      </h1>
+    </div>
+  )
+}
+
+export default App
