766cad6cd6adc404b74313ca625efa6155a75c98
VanrougeK <ochoavania795@gmail.com>
2026-03-12 20:02:13 -0600

    Fix galeria.jsx responsive
    Mas img y vid agregados a la galeria y modificacion para ser responsivo

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -3,21 +3,24 @@ import React from "react";
 
 export default function Galeria() {
   const items = [
-    { id: 1, info: "Dibujo 1", img: "/img/Scarlett.png" },
-    { id: 2, info: "Dibujo 2", img: "/img/furro.png" },
-    { id: 3, info: "Dibujo 3", img: "/img/quackity.png" },
+    { id: 1, info: "Dibujo 1", img: "/img/Scarlett.png", type: "img" },
+    { id: 2, info: "Dibujo 2", img: "/img/furro.png", type: "img" },
+    { id: 3, info: "Dibujo 3", img: "/img/quackity.png", type: "img" },
+    { id: 4, info: "Dibujo 1", img: "/img/qsmp_2.png", type: "img" },
+    { id: 5, info: "Dibujo 2", img: "/img/mmmmm ok.png", type: "img" },
+    { id: 6, info: "Video 1", video: "/video/test.mp4", type: "video" },
   ];
   return (
     <section
       id="galeria"
-      className="px-12 py-10"
+      className="px-4 md:px-12 py-10"
       style={{
         background:
           "linear-gradient(135deg, #ffb3d9, #c9b3ff, #b3d9ff, #b3ffda)",
       }}
     >
       <h2
-        className="text-2xl font-bold mb-8"
+        className="text-lg md:text-2xl font-bold mb-8"
         style={{
           color: "#fff",
           fontFamily: "",
@@ -27,18 +30,28 @@ export default function Galeria() {
         ✦ Galeria
       </h2>
 
-      <div className="grid grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {items.map((item) => (
           <div
             key={item.id}
             className="rounded-2xl overflow-hidden"
-            style={{ background: "rgba(255, 255, 255, 0.4" }}
+            style={{ background: "rgba(255, 255, 255, 0.4)" }}
           >
-            <img
-              className="w-full h-56 object-cover"
-              src={item.img}
-              alt={item.img}
-            />
+            {item.type === "video" ? (
+              <video
+                autoPlay
+                src={item.video}
+                className="w-full h-56 object-cover"
+                controls
+              />
+            ) : (
+              <img
+                src={item.img}
+                alt={item.info}
+                className="w-full h-56 object-cover"
+              />
+            )}
+
             <p
               className="text-xs text-center py-3 font-bold"
               style={{ color: "#a070c0" }}
