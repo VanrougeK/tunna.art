7d14503d613e55444119b320de19ed258db22270
VanrougeK <ochoavania795@gmail.com>
2026-07-08 21:26:43 -0600

    Modificacion para un reporte de pruebas en software con Yadhira jeje

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -25,8 +25,8 @@ export default function Contacto() {
         "tBby89lt-Nlp0I6Mm",
       )
       await emailjs.send(
-        "service_90tsrwu",
-        "template_q132dtd",
+        "service_FAKE123",
+        "template_FAKE123",
         {
           email:form.email,
           service: form.service,
